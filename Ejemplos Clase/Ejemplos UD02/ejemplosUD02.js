05810655d02a32ad72a36045c79587cc7ad32fe1
PacoAranda <franciscojavier.aranda.alu@iescampanillas.com>
2021-09-23 10:33:24 +0200

    actualización

---
+++ Ejemplos Clase/Ejemplos UD02/ejemplosUD02.js
@@ -0,0 +1,13 @@
+function ejemplo22(){
+    console.log("Esto es un mensaje en la consola");
+    alert("Esto es un mensaje en una ventana emergente");
+}
+function ejemplo32(){
+    var ejemplo=3 //Si no se pone nada, cuenta como var (ejemplo=3)
+    if (ejemplo==3){
+        var variable1 = 7;
+        let variable2 = 2;
+    }
+    console.log("La variable 1 vale "+variable1); //variable1 existe en este lugar
+    console.log("La variable 2 vale "+variable2); //variable2 no existe en este lugar
+}
