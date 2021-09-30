a415277e3bb07bdc2538490d3c7dcb74792ae553
PacoAranda <franciscojavier.aranda.alu@iescampanillas.com>
2021-09-30 09:03:20 +0200

    actualizacion

---
+++ Ejemplos Clase/Ejemplos UD02/Apartados 4-5/ejemplosUD02_45.js
@@ -0,0 +1,9 @@
+function ejemplo51(){
+    let num1 = 3;
+    let num2 = 5;
+    console.log(num2+=num1);
+    console.log(num2-=num1);
+    console.log(num2*=num1);
+    console.log(num2/=num1);
+    console.log(num2%=num1);
+}
